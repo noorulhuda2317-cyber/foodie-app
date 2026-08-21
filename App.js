e8566e68c3a9e2bc1c29febe2089648b2a1574d5
noorulhuda2317-cyber <noorulhuda2317@gmail.com>
2026-08-22 00:55:38 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,26 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-
-import { AppProvider } from './src/context/AppContext';
-import MainFeedScreen from './src/screens/MainFeedScreen';
-import RecipeDetailScreen from './src/screens/RecipeDetailScreen';
-import FavoritesScreen from './src/screens/FavoritesScreen';
-import MyRecipesScreen from './src/screens/MyRecipesScreen';
-import AddEditRecipeScreen from './src/screens/AddEditRecipeScreen';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <AppProvider>
-      <StatusBar style="dark" />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="MainFeed"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="MainFeed" component={MainFeedScreen} />
-          <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
-          <Stack.Screen name="Favorites" component={FavoritesScreen} />
-          <Stack.Screen name="MyRecipes" component={MyRecipesScreen} />
-          <Stack.Screen
-            name="AddEditRecipe"
-            component={AddEditRecipeScreen}
-            options={{ headerShown: true }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AppProvider>
-  );
+{
+  "expo": {
+    "name": "Foodie",
+    "slug": "foodie-app",
+    "version": "1.0.0",
+    "sdkVersion": "54.0.0",
+    "orientation": "portrait",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": false,
+    "splash": {
+      "backgroundColor": "#FFF6ED"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "backgroundColor": "#FFF6ED"
+      }
+    },
+    "web": {
+      "bundler": "metro"
+    }
+  }
 }
